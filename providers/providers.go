0d68b05eadafaca9fbaa9f997fad703fb43b7c7a
JFC <jfc@responsiveconsulting.fr>
2018-08-18 17:03:30 +0200

    fix: linter warnings and add comments

--- providers/providers.go
+++ providers/providers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/gob"
 )
 
+// Provider is the interface for a provider
 type Provider interface {
 	Name() string                           // Provider's name
 	Shows([]*MatchRequest) ([]*Show, error) // List of available shows that match one of MatchRequest
@@ -12,16 +13,19 @@ type Provider interface {
 	GetShowFileNameMatcher(*Show) string    // Give a file name matcher for searching duplicates having different episode number
 }
 
+// Configurer interface
 type Configurer interface {
 	SetConfig(map[string]interface{})
 }
 
 var providers = map[string]Provider{}
 
+// Register is called by provider's init to register the provider
 func Register(p Provider) {
 	providers[p.Name()] = p
 }
 
+// List of registered providers
 func List() map[string]Provider {
 	return providers
 }
