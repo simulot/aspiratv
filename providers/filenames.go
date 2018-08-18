e40519443368ee0ff1b23326e139748d0d33fa17
JFC <jfc@responsiveconsulting.fr>
2018-08-18 17:03:30 +0200

    fix: linter warnings and add comments

--- providers/filenames.go
+++ providers/filenames.go
@@ -7,10 +7,12 @@ var (
 	pathNameReplacer = strings.NewReplacer("!", "", "?", "", ":", "-", ",", "", "*", "", "|", "-", "\"", "", ">", "", "<", "")
 )
 
+// FileNameCleaner return a safe file name from a given show name.
 func FileNameCleaner(s string) string {
 	return strings.TrimSpace(fileNameReplacer.Replace(s))
 }
 
+// PathNameCleaner return a safe path name from a given show name.
 func PathNameCleaner(s string) string {
 	if i := strings.Index(s, ":"); i >= 0 && i < 2 {
 		return s[:i] + strings.TrimSpace(pathNameReplacer.Replace(s[i:]))
@@ -18,6 +20,7 @@ func PathNameCleaner(s string) string {
 	return strings.TrimSpace(pathNameReplacer.Replace(s))
 }
 
+// Format2Digits return a number with 2 digits when there is only one digit
 func Format2Digits(d string) string {
 	if len(d) < 2 {
 		return "0" + d
