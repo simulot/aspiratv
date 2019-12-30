347f9b59aafe886d4dec6435a8b87fc7b51d0e52
Mickaël Canévet <mickael.canevet@camptocamp.com>
2019-12-30 11:44:21 +0100

    Fix argument description (#33)

--- cmd/aspiratv/main.go
+++ cmd/aspiratv/main.go
@@ -99,7 +99,7 @@ func main() {
 	flag.StringVar(&a.Config.ConfigFile, "config", "config.json", "Configuration file name.")
 	flag.IntVar(&a.Config.ConcurrentTasks, "max-tasks", runtime.NumCPU(), "Maximum concurrent downloads at a time.")
 	flag.StringVar(&a.Config.Provider, "provider", "", "Provider to be used with download command. Possible values : artetv,francetv,gulli")
-	flag.StringVar(&a.Config.Destination, "destination", "", "Provider to be used with download command. Possible values : artetv,francetv,gulli")
+	flag.StringVar(&a.Config.Destination, "destination", "", "Destination path.")
 	flag.StringVar(&a.Config.LogFile, "log", "", "Give the log file name. When empty, no log.")
 	// flag.IntVar(&a.Config.RetentionDays, "retention", 0, "Delete media older than retention days for the downloaded show.")
 	flag.BoolVar(&a.Config.WriteNFO, "write-nfo", true, "Write NFO file for KODI,Emby,Plex...")
