7f01d6574ec67ac80363ed44903575de97d2de39
simulot <jfc@responsiveconsulting.fr>
2020-04-16 21:07:16 +0200

    Add MPD filters for FRANCE TV

--- go.mod
+++ go.mod
@@ -4,20 +4,27 @@ go 1.12
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.0 // indirect
+	github.com/PuerkitoBio/purell v1.1.1
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/alecthomas/participle v0.3.0
 	github.com/antchfx/htmlquery v1.0.0 // indirect
 	github.com/antchfx/xmlquery v1.0.0 // indirect
 	github.com/antchfx/xpath v1.0.0 // indirect
+	github.com/gnewton/chidley v0.0.0-20180421200106-b4c6c155522e // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
 	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/go-cmp v0.4.0
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mc2soft/mpd v0.0.0-20200127151228-9f4321766fef
 	github.com/pkg/errors v0.9.1
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/sergi/go-diff v1.1.0
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	github.com/vbauerster/mpb/v4 v4.9.2
+	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	golang.org/x/crypto v0.0.0-20190829043050-9756ffdc2472 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8
 	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297 // indirect
---
+++ parsers/mpdparser/mpdparser.go
@@ -0,0 +1,202 @@
+package mpdparser
+
+import (
+	"context"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/purell"
+)
+
+// MPDParser holds the MPD structure
+type MPDParser struct {
+	*MPD
+	ActualURL string
+}
+
+// NewMPDParser allocate a new MPD parser
+func NewMPDParser() *MPDParser {
+	return &MPDParser{}
+}
+
+// Get queries the webserver, remember the redirected location and get the MPD
+func (p *MPDParser) Get(ctx context.Context, url string) error {
+	// Set up the HTTP request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	// disable redirection system using the simple vanilla roundtripper
+	transport := http.Transport{}
+	resp, err := transport.RoundTrip(req)
+	if err != nil {
+		return err
+	}
+	// Check if you received the status codes you expect.
+	if resp.StatusCode >= 400 {
+		return fmt.Errorf("Failed with status: %q", resp.Status)
+	}
+
+	if resp.StatusCode >= 300 {
+		// Get the new location, and jump on it
+		p.ActualURL = resp.Header.Get("Location")
+		resp.Body.Close()
+
+		resp, err = http.Get(p.ActualURL)
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode >= 400 {
+			return fmt.Errorf("Failed with status: %q", resp.Status)
+		}
+	}
+
+	return p.Read(resp.Body)
+}
+
+// Read the MPD from the reader and close it
+func (p *MPDParser) Read(rc io.ReadCloser) error {
+	mpd := &MPD{}
+	b, err := ioutil.ReadAll(rc)
+	if err != nil {
+		return err
+	}
+	defer rc.Close()
+
+	err = xml.Unmarshal(b, mpd)
+	if err != nil {
+		return fmt.Errorf("Can't unmarshal MPD: %w", err)
+	}
+
+	p.MPD = mpd
+	return nil
+}
+
+// Write the MPD to the writer and close it
+func (p *MPDParser) Write(wc io.WriteCloser) error {
+	defer wc.Close()
+	output, err := xml.Marshal(p.MPD)
+	if err != nil {
+		return fmt.Errorf("Can't marshal MPD: %w", err)
+	}
+
+	_, err = wc.Write([]byte(xml.Header))
+	if err != nil {
+		return fmt.Errorf("Can't marshal MPD: %w", err)
+	}
+	_, err = wc.Write(output)
+	if err != nil {
+		return fmt.Errorf("Can't write MPD: %w", err)
+	}
+	return nil
+}
+
+// StripSTPPStream isn't correctly handeld by FFMPEG at writing time
+func (p *MPDParser) StripSTPPStream() error {
+	for _, pe := range p.MPD.Period {
+		newAS := []*AdaptationSet{}
+		for _, a := range pe.AdaptationSet {
+			if a.MimeType == "application/mp4" && a.Codecs == "stpp" {
+				continue
+			}
+			newAS = append(newAS, a)
+		}
+		pe.AdaptationSet = newAS
+	}
+	return nil
+}
+
+// AbsolutizeURLs change relative urls to absolute
+// Either there is a BaseURL, and we have to make it absolute. In this case, segment templates should be relative
+// Either there isn't BasURL. Then if Segments templates are relative to MDP's URI
+// Check BaseURL or SegmentTemplates
+func (p *MPDParser) AbsolutizeURLs(base string) error {
+	for _, p := range p.MPD.Period {
+		if len(p.BaseURL) > 0 {
+
+			u, err := url.Parse(p.BaseURL)
+			if err != nil {
+				return fmt.Errorf("Can't parse MPD BaseURL: %w", err)
+			}
+			if !u.IsAbs() {
+				p.BaseURL, err = changeURL(base, p.BaseURL)
+				if err != nil {
+					return err
+				}
+			}
+			continue
+		}
+		for _, a := range p.AdaptationSet {
+			if len(a.SegmentTemplate.Initialization) > 0 {
+				u, err := changeURL(base, a.SegmentTemplate.Initialization)
+				if err != nil {
+					return fmt.Errorf("Can't process url change: %w", err)
+				}
+				a.SegmentTemplate.Initialization = u
+			}
+			if len(a.SegmentTemplate.Media) > 0 {
+				u, err := changeURL(base, a.SegmentTemplate.Media)
+				if err != nil {
+					return fmt.Errorf("Can't process url change: %w", err)
+				}
+				a.SegmentTemplate.Media = u
+			}
+		}
+
+	}
+	return nil
+}
+
+func changeURL(base, URL string) (string, error) {
+	u, err := url.Parse(URL)
+	if err != nil {
+		return "", err
+	}
+	if u.IsAbs() {
+		return URL, nil
+	}
+	return purell.NormalizeURLString(base+"/"+URL, purell.FlagsUsuallySafeGreedy)
+}
+
+// KeepBestVideoStream discard all video stream but the one with the highest bandwidth
+func (p *MPDParser) KeepBestVideoStream() error {
+	for _, p := range p.MPD.Period {
+		for _, a := range p.AdaptationSet {
+			if !strings.HasPrefix(a.MimeType, "video/") {
+				continue
+			}
+
+			bandwith := 0
+			best := -1
+			for i, r := range a.Representation {
+				b, err := strconv.Atoi(r.Bandwidth)
+				if err != nil {
+					continue
+				}
+				if b > bandwith {
+					bandwith = b
+					best = i
+				}
+			}
+
+			if best == -1 {
+				return nil
+			}
+			a.Representation = []*Representation{
+				a.Representation[best],
+			}
+			a.MinBandwidth = a.Representation[0].Bandwidth
+			a.MaxBandwidth = a.Representation[0].Bandwidth
+			a.MaxWidth = a.Representation[0].Width
+			a.MaxHeight = a.Representation[0].Height
+		}
+	}
+	return nil
+}
